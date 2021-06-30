2efb68c69768c254ea55b36afbef2f8804351dd1
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-06-30 14:01:50 -0400

    Agregando contructor a la clase cobros

--- Parcial2-ap2-2018-0553/Models/Cobros.cs
+++ Parcial2-ap2-2018-0553/Models/Cobros.cs
@@ -20,5 +20,17 @@ namespace Parcial2_ap2_2018_0553.Models
 
         [ForeignKey("CobroId")]
         public virtual List<CobrosDetalle> Detalle { get; set; }
+
+        public Cobros()
+        {
+            CobroId = 0;
+            Fecha = DateTime.Now;
+            Totales = 0;
+            TotalCobro = 0;
+            Observaciones = string.Empty;
+            ClienteId = 0;
+            Cliente = new Clientes();
+            Detalle = new List<CobrosDetalle>();
+        }
     }
 }
